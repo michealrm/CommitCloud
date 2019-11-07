5ccb4316fb7f00b1e24225b6721b3a5957a3c653
Micheal Myers <michealrmyers@gmail.com>
2019-11-06 23:52:24 -0600

    Bootstrapped with create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h1>CommitCloud</h1>
+        <h3>Word clouds for commits across GitHub via <a href="https://gharchive.org/">GH Archive</a></h3>
+      </div>
+    );
+}
+
+export default App;
