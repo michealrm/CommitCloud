b17af42d7e2012f8cd01067d5511ce0cf95a1311
Micheal Myers <michealrmyers@gmail.com>
2019-11-07 19:11:55 -0600

    Project submission

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import Async from 'react-async';
+import jsonData from './jsonData2.json';
+import ReactWordcloud from 'react-wordcloud';
 const {BigQuery} = require('@google-cloud/bigquery');
 const bq = new BigQuery();
 
 function App() {
   const [msg, setMsg] = useState('Loading...');
+  const [started, setStarted] = useState(null);
+  const [data, setData] = useState([]);
 
 async function query() {
 
@@ -20,6 +24,7 @@ async function query() {
     const options = {
         query: query,
         location: 'US',
+        timeoutMS: 10000
     };
 
     const [job] = await bq.createQueryJob(options);
@@ -30,22 +35,87 @@ async function query() {
     return rows;
 }
 
+function mapWordCounts (wordsMap, wordsArray) {
+
+  wordsArray.forEach(key => {
+    if (wordsMap.hasOwnProperty(key)) {
+      wordsMap[key]++;
+    } else {
+      wordsMap[key] = 1;
+    }
+  });
+}
+
+function range(start, stop, step) {
+    if (typeof stop == 'undefined') {
+        // one param defined
+        stop = start;
+        start = 0;
+    }
+
+    if (typeof step == 'undefined') {
+        step = 1;
+    }
+
+    if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+        return [];
+    }
+
+    var result = [];
+    for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+        result.push(i);
+    }
+
+    return result;
+};
+
+function get_commits(limit = 100) {
+    var allCommits = [];
+    var wordsMap = {};
+    jsonData.forEach(line => {
+        if(line.type === "PushEvent") {
+            var commits = line.payload.commits;
+            commits.forEach(c => {
+                mapWordCounts(wordsMap, c.message.split(' '))
+            });
+        }
+    });
+
+    Object.keys(wordsMap).forEach(key => {
+        allCommits.push({text:key, value:wordsMap[key]});
+    });
+    allCommits.sort((a, b) => (a.value < b.value ? 1 : -1));
+    console.log(allCommits);
+    if(limit > 0)
+        allCommits = allCommits.slice(0, limit);
+    return allCommits;
+}
+
+if(!started) {
+    setStarted(true);
+    var d = get_commits();
+    setData(d);
+}
+if(data.length == 0) {
+    return ("Loading...");
+} else {
   return (
       <div>
         <h1>CommitCloud</h1>
         <h3>Word clouds for commits across GitHub via <a href="https://gharchive.org/">GH Archive</a></h3>
-        <Async promiseFn={query}>
-            <Async.Pending>{ msg }</Async.Pending>
-            // TODO: Need to add rejected check here
-            <Async.Fulfilled>
-                <h6>Data</h6>
-                {data => (
-                    <p>{data}</p>
-                )}
-            </Async.Fulfilled>
-        </Async>
+      <div style={{width:'900px',height:'900px'}}>
+        <ReactWordcloud
+            options={{
+                rotations: 2,
+                rotationAngles: [0, 90],
+                fontSizes: range(15, 400, 100),
+            }}
+            words={data}
+      />
+      </div>
       </div>
     );
+}
 }
 
 export default App;
