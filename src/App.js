0cc29bfeffbbc09d4836f1a6c69d5afa1d23110b
Micheal Myers <michealrmyers@gmail.com>
2019-11-07 22:24:59 -0600

    Used local storage to save word counts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
-import Async from 'react-async';
 import jsonData from './jsonData2.json';
 import ReactWordcloud from 'react-wordcloud';
 const {BigQuery} = require('@google-cloud/bigquery');
 const bq = new BigQuery();
 var nlp = require('compromise')
+const useStateWithLocalStorage = localStorageKey => {
+    var stored = localStorage.getItem(localStorageKey);
+    var parsed = null;
+    try {
+        parsed = JSON.parse(stored);
+    } catch(error) {}
+    const [value, setValue] = React.useState(parsed);
+    React.useEffect(() => {
+        localStorage.setItem(localStorageKey, JSON.stringify(value));
+    }, [value]);
 
-function App() {
-  const [msg, setMsg] = useState('Loading...');
-  const [started, setStarted] = useState(null);
-  const [data, setData] = useState([]);
+    return [value, setValue];
+};
 
 async function query() {
 
@@ -29,34 +36,11 @@ async function query() {
     };
 
     const [job] = await bq.createQueryJob(options);
-    setMsg(`Job ${job.id} started. Waiting to process results.`);
-    console.log(msg);
+    console.log(`Job ${job.id} started. Waiting to process results.`);
 
     const [rows] = await job.getQueryResults();
     return rows;
 }
-
-function range(start, stop, step) {
-    if (typeof stop == 'undefined') {
-        stop = start;
-        start = 0;
-    }
-
-    if (typeof step == 'undefined') {
-        step = 1;
-    }
-
-    if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
-        return [];
-    }
-
-    var result = [];
-    for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
-        result.push(i);
-    }
-
-    return result;
-};
 
 function get_commits(limit = 100) {
     var allCommits = [];
@@ -68,6 +52,7 @@ function get_commits(limit = 100) {
         }
     });
     var nouns = nlp(commits).sentences().nouns().out('array');
+    console.log(nouns);
     nouns.forEach(key => {
         if(wordsMap.hasOwnProperty(key)) {
             wordsMap[key]++;
@@ -86,31 +71,33 @@ function get_commits(limit = 100) {
     return allCommits;
 }
 
-if(!started) {
-    setStarted(true);
-    var d = get_commits();
-    setData(d);
-}
-if(data.length == 0) {
-    return ("Loading...");
-} else {
-  return (
-      <div>
-        <h1>CommitCloud</h1>
-        <h3>Word clouds for commits across GitHub via <a href="https://gharchive.org/">GH Archive</a></h3>
-      <div style={{width:'800px',height:'800px'}}>
-        <ReactWordcloud
-            options={{
-                rotations: 3,
-                rotationAngles: [0, 90],
-                fontSizes: [15,100,200,300,400],
-            }}
-            words={data}
-      />
-      </div>
-      </div>
-    );
-}
+
+function App() {
+
+  const [data, setData] = useStateWithLocalStorage('data');
+
+    if(!data) {
+        setData(get_commits())
+        return ('Processing...');
+    } else {
+        console.log(data);
+      return (
+          <div className="container">
+            <h1>CommitCloud</h1>
+            <h3>Word clouds for commits across GitHub via <a href="https://gharchive.org/">GH Archive</a></h3>
+          <div style={{width:'800px',height:'800px'}}>
+            <ReactWordcloud
+                options={{
+                    rotations: 3,
+                    rotationAngles: [0, 90],
+                    fontSizes: [15,100,200,300,400],
+                }}
+                words={data}
+          />
+          </div>
+          </div>
+        );
+    }
 }
 
 export default App;
